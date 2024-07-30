f7dd63f37f0dd835dd64e69ed6971f907698507d
alissav <63168432+alissav30@users.noreply.github.com>
2024-07-30 13:52:53 -0700

    cleaning it up

--- src/main.jsx
+++ src/main.jsx
@@ -42,79 +42,15 @@ function webglAvailable() {
       document.getElementById('webgl-warning').style.display = 'block';
     }
 
-//const renderer = new THREE.WebGLRenderer({
-//    canvas: document.querySelector('#bg'),
-//})
-
-//renderer.setPixelRatio(window.devicePixelRatio);
-//renderer.setSize(window.innerWidth, window.innerHeight);
-//camera.position.setZ(30);
-//camera.position.setX(-10);
-
-
-
-//renderer.render(scene, camera);
-
-//const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-//const torusTexture = new THREE.TextureLoader().load('abstract-bg.jpg');
-
-//const material = new THREE.MeshStandardMaterial({ map: torusTexture });
-//const torus = new THREE.Mesh(geometry, material);
-//torus.position.z = -2;
-//scene.add(torus);
-
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
-//const lightHelper = new THREE.PointLightHelper(pointLight);
-//const gridHelper = new THREE.GridHelper(200, 50);
-//scene.add(lightHelper, gridHelper);
-
 const controls = new OrbitControls(camera, renderer.domElement);
-
-//const capsules = [];
-
-//function addCapsule1() {
-//    //const geometry = new THREE.SphereGeometry(0.25);
-//    //const geometry = new THREE.OctahedronGeometry(1.5);
-
-
-//    const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 ); 
-
-//    const textures = [
-//        'ethiopian-flag.jpg',
-//        'capsule-texture-3.jpeg',
-//        'capsule-pride.jpeg'
-//    ];
-    
-//    const randomTexturePath = textures[Math.floor(Math.random() * textures.length)];
-//    const capsuleTexture = new THREE.TextureLoader().load(randomTexturePath);
-
-//    //const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
-//    const material = new THREE.MeshBasicMaterial( { map: capsuleTexture })
-//    const capsule = new THREE.Mesh(geometry, material);
-
-//    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(120))
-
-//    const pointLight = new THREE.PointLight(0xffffff);
-//    pointLight.position.set(x, y, z);
-//    scene.add(pointLight);
-
-//    capsule.position.set(x, y, z);
-//    scene.add(capsule);
-
-//    capsules.push(capsule); // Add the capsule to the array
-//}
-
-//Array(100).fill().forEach(addCapsule1);
 
 const paperTexture = new THREE.TextureLoader().load('/assets/skyline-13.jpg');
 scene.background = paperTexture;
-
-// Load skyscraper texture
-//const skyscraperTexture = new THREE.TextureLoader().load('skyscraper-texture-new-5.jpg');
 
 const skyscraperTextures = [
     new THREE.TextureLoader().load('/assets/skyscraper-texture-new-8.jpg'),
@@ -123,13 +59,6 @@ const skyscraperTextures = [
     new THREE.TextureLoader().load('/assets/skyscraper-texture-4.jpg'),
 ]
 
-//const billboardTextures = [
-//    new THREE.TextureLoader().load('billboard-gif-1.gif'),
-//    new THREE.TextureLoader().load('billboard-gif-2.gif'),
-//    new THREE.TextureLoader().load('billboard-gif-3.gif'),
-//    new THREE.TextureLoader().load('billboard-gif-4.gif'),
-//    new THREE.TextureLoader().load('billboard-gif-5.gif'),
-//];
 
 const videoFiles = [
     '/assets/billboard-gif-1.mp4',
@@ -193,15 +122,7 @@ for (let i = 0; i < 60; i++) {
     billboards.push(billboard);
 }
 
-//const alissaTexture = new THREE.TextureLoader().load('alissa-pic.jpg');
-//const alissa = new THREE.Mesh(
-//    new THREE.BoxGeometry(4, 4, 4),
-//    new THREE.MeshBasicMaterial( { map: alissaTexture })
-//)
-//scene.add(alissa);
-
 // Load and position Alissa's picture
-
 const alissaTexture = new THREE.TextureLoader().load('/assets/alissa-pic-10.png');
 const alissaBillboardGeometry = new THREE.BoxGeometry(21, 21, 21);
 const alissaBillboardMaterial = new THREE.MeshStandardMaterial({ map: alissaTexture});
@@ -215,22 +136,6 @@ const edges = new THREE.EdgesGeometry(alissaBillboardGeometry);
 const neonMaterial = new THREE.LineBasicMaterial({ color: 0xe8a0be, linewidth: 5 });
 const neonEdges = new THREE.LineSegments(edges, neonMaterial);
 alissaBillboard.add(neonEdges);
-
-//// Add a spotlight to illuminate the Alissa billboard
-//const spotlight = new THREE.SpotLight(0xffffff, 50); // Color and intensity (brighter light)
-//spotlight.position.set(30, 15, 10); // Adjusted position
-//spotlight.angle = Math.PI / 3; // Spread angle
-//spotlight.penumbra = 0.3; // Softer edges
-//spotlight.decay = 2; // Light decay
-//spotlight.distance = 200; // Distance the light covers
-//spotlight.castShadow = true;
-
-//// Set the spotlight target to the Alissa billboard
-//spotlight.target = alissaBillboard;
-
-//// Add the spotlight and its target to the scene
-//scene.add(spotlight);
-//scene.add(spotlight.target);
 
 // Add a RectAreaLight to illuminate the Alissa billboard
 const rectLight = new THREE.RectAreaLight(0xffffff, 0.6, 70, 70); // Color, intensity, width, height
@@ -318,9 +223,6 @@ function animateRocket() {
         car.position.x = radius * Math.cos(rocketAngle);
         car.position.z = -2 * radius * Math.sin(rocketAngle);
         car.position.y = 30 + 12 * Math.sin(rocketAngle * 2); 
-        //car.rotation.y = -rocketAngle * 0.01; 
-        //car.rotation.z = -rocketAngle * 0.01; 
-        //car.rotation.x = -rocketAngle * 0.01; 
     });
 }
 
@@ -368,44 +270,6 @@ function moveCarOnScroll() {
     }
 }
 
-//camera.position.z = 30 + t * -0.0095;
-//camera.position.x = -10 + t * -0.0005;
-//camera.position.y = -5 + t * -0.00025;
-
-
-//document.body.onscroll = moveCarOnScroll;
-
-
-// Add multiple cars
-//for (let i = 0; i < 1; i++) {
-//    addCar(Math.random() * 50 - 25, -49.5, Math.random() * 50 - 25);
-//}
-
-// Function to animate cars
-//function animateCars() {
-//    cars.forEach(car => {
-//        car.position.x += 0.1; // Move car along x-axis
-//        if (car.position.x > 50) car.position.x = -50; // Reset position if out of bounds
-//    });
-//}
-
-//const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-//const normalTexture = new THREE.TextureLoader().load('normal.jpg');
-
-//const moon = new THREE.Mesh(
-//    new THREE.SphereGeometry(3, 32, 32),
-//    new THREE.MeshStandardMaterial({ 
-//        map: moonTexture,
-//        normalMap: normalTexture
-//    })
-//)
-//moon.position.z = 30;
-//moon.position.x = -10;
-//scene.add(moon);
-
-//alissa.position.z = -6;
-//alissa.position.x = 1;
-
 // Function to change the billboard texture
 function changeBillboardTexture() {
     billboards.forEach(billboard => {
@@ -426,10 +290,6 @@ let prevScrollY = window.scrollY;
 function moveCamera() {
     const t = document.body.getBoundingClientRect().top;
     const scrollDelta = window.scrollY - prevScrollY;
-
-    //moon.rotation.x += 0.05;
-    //moon.rotation.y += 0.075;
-    //moon.rotation.z += 0.05;
 
     alissaBillboard.rotation.x -= 0.017;
     alissaBillboard.rotation.y += 0.037;
@@ -463,18 +323,7 @@ moveCamera()
 function animate() {
     requestAnimationFrame(animate);
 
-    //torus.rotation.x += 0.02;
-    //torus.rotation.y += 0.005;
-    //torus.rotation.z += 0.02;
-
-    //capsules.forEach(capsule => {
-    //    capsule.rotation.x += 0.01;
-    //    capsule.rotation.y += 0.01;
-    //    capsule.rotation.z += 0.01;
-    //});
-
-    animateRocket(); // Animate the rocket
-    //animateCar()
+    animateRocket(); 
 
     controls.update();
 
